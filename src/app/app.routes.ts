73448ccfaae8f047dc6fe19c1291b5eb2d829c7f
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-18 18:52:43 +0200

    routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', 
+        loadChildren: () => import('./book_library_app/book_library.module').then(m => m.BookLibraryModule)
+    }
+];
