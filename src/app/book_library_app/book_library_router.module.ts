2f51990ee256e36b5b2d4c1555ac44a83a09d8e3
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-21 11:14:13 +0200

    added homepage route

--- src/app/book_library_app/book_library_router.module.ts
+++ src/app/book_library_app/book_library_router.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { AuthComponent } from './auth/auth.component'
+import { BookLibraryHomepageComponent } from './book-library-homepage/book-library-homepage.component'
 
 const routes: Routes = [
-    { path: '', component: AuthComponent }
+    { path: '', component: AuthComponent },
+    { path: 'homepage', component: BookLibraryHomepageComponent }
 ]
 
 @NgModule({
