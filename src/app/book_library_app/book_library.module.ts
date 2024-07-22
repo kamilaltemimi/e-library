e1230b684aa5411323068e26142b6b9aa31e6f55
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-22 22:53:38 +0200

    components declared and configured module by adding reactive forms module

--- src/app/book_library_app/book_library.module.ts
+++ src/app/book_library_app/book_library.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
-import { AuthComponent } from './auth/auth.component'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { BookLibraryRouterModule } from './book_library_router.module'
+
+import { AuthComponent } from './auth/auth.component'
 import { BookLibraryHomepageComponent } from './book-library-homepage/book-library-homepage.component';
+import { BooksComponent } from './books/books.component';
+import { AddBookComponent } from './books/add-book/add-book.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 
 @NgModule({
     declarations: [
         AuthComponent,
-        BookLibraryHomepageComponent
+        BookLibraryHomepageComponent,
+        BooksComponent,
+        AddBookComponent,
+        BookDetailsComponent,
+        ProfileComponent
     ],
     imports: [
         CommonModule,
         BookLibraryRouterModule,
-        SharedModule
+        SharedModule,
+        ReactiveFormsModule
     ],
     exports: []
 })
