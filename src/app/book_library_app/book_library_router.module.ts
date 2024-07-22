42242be31e61f70df02133b771467feaa0732b2b
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-22 22:53:05 +0200

    paths added

--- src/app/book_library_app/book_library_router.module.ts
+++ src/app/book_library_app/book_library_router.module.ts
@@ -3,10 +3,17 @@ import { RouterModule, Routes } from '@angular/router'
 
 import { AuthComponent } from './auth/auth.component'
 import { BookLibraryHomepageComponent } from './book-library-homepage/book-library-homepage.component'
+import { ProfileComponent } from './profile/profile.component'
+import { BooksComponent } from './books/books.component'
+import { AddBookComponent } from './books/add-book/add-book.component'
 
 const routes: Routes = [
     { path: '', component: AuthComponent },
-    { path: 'homepage', component: BookLibraryHomepageComponent }
+    { path: 'homepage', component: BookLibraryHomepageComponent },
+    { path: 'profile', component: ProfileComponent},
+    { path: 'books', component: BooksComponent },
+    { path: 'books/add-book', component: AddBookComponent },
+    { path: '**', redirectTo: 'auth' }
 ]
 
 @NgModule({
