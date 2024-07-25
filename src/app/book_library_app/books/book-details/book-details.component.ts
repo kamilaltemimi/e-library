8501504db3c795fabd3e741523d58673a4d831ee
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-25 22:48:48 +0200

    setted up book-details component

---
+++ src/app/book_library_app/books/book-details/book-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-details',
+  standalone: false,
+  templateUrl: './book-details.component.html',
+  styleUrl: './book-details.component.scss'
+})
+export class BookDetailsComponent {
+
+}
