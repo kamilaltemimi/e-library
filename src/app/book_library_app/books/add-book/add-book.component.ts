dc0e371c954ce7ce974172313d08a556bbcf251c
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-25 22:47:40 +0200

    worked on add-book component. completed form (working properly) Later I am going to add error handling etc

--- src/app/book_library_app/books/add-book/add-book.component.ts
+++ src/app/book_library_app/books/add-book/add-book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsersService } from '../../../core/services/users.service';
 import { User } from '../../../core/models/user';
+import { BooksService } from '../../../core/services/books.service';
 
 @Component({
   selector: 'app-add-book',
@@ -14,16 +15,18 @@ export class AddBookComponent implements OnInit {
   addBookForm!: FormGroup
   activeUser!: User
   publicationYearOptions: number[] = []
+  selectedFile!: File
 
   constructor(
     private fb: FormBuilder,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private booksService: BooksService
   ) {}
 
   ngOnInit(): void {
     this.activeUser = this.usersService.activeUser.value!;
     this.initializeForm();
-    for (let i = 1900; i <= 2024; i ++) {
+    for (let i = 1901; i <= 2024; i ++) {
       this.publicationYearOptions.push(i)
     }
   }
@@ -31,10 +34,25 @@ export class AddBookComponent implements OnInit {
 
   initializeForm(): void {
     this.addBookForm = this.fb.group({
-      title: ['', Validators.required],
-      author: ['', Validators.required],
+      title: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      description: ['', [Validators.required]],
       publicationYear: [null, Validators.required],
-      addedBy: [this.activeUser.user_id, Validators.required],
+      addedBy: [this.activeUser.user_id, [Validators.required]],
+      bookImage: [null, [Validators.required]]
     })
   }
+
+  onFileChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+    }
+  }
+  
+
+  submitAddBookForm(): void {
+      this.booksService.addNewBook({...this.addBookForm.value, bookImage: this.selectedFile.name}).subscribe(data => console.log(data))
+  }
+  
 }
