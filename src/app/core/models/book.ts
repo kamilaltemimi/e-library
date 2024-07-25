e5ae72ab1bd1ab83b226598e3a2cd6110a966ff4
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-25 22:47:55 +0200

    added book interface

---
+++ src/app/core/models/book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    added_by: number,
+    title: string,
+    author: string,
+    description: string,
+    publication_year: number,
+    createdAt?: number,
+    book_image: string,
+    book_id?: number
+}
