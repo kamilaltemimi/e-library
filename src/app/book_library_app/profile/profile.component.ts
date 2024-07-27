cc8d09a9f83fcb5c1bbaa641513eab45e6be2da2
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-27 19:34:53 +0200

    worked on profile component - prepared responsive layout and implemented methods to fetch data from backend

--- src/app/book_library_app/profile/profile.component.ts
+++ src/app/book_library_app/profile/profile.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../core/models/user';
+import { UsersService } from '../../core/services/users.service';
+import { firstValueFrom, map } from 'rxjs';
+import { BooksService } from '../../core/services/books.service';
+import { Book } from '../../core/models/book';
+import { BorrowedBook } from '../../core/models/borrowed-book';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +12,40 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  activeUser?: User
+  borrowedBooks?: Book[]
+
+  constructor(
+    private usersService: UsersService,
+    private booksService: BooksService
+  ){}
+
+  ngOnInit(): void {
+    this.getActiveUser()
+    this.getBorrowedBooks()
+  }
+
+  getActiveUser(): void {
+    this.usersService.activeUser.pipe(map((data: User | null) => {
+      if (data) {
+        const date = data?.created_at?.split('T')[0]
+        const newUser: User = {...data, created_at: date}
+        return newUser
+      } else return
+    })).subscribe((data: User | undefined) => {
+      this.activeUser = data
+    })
+  }
+
+  getBorrowedBooks(): void {
+    this.booksService.getBorrowedBooks(this.activeUser!.user_id).subscribe((books: Book[]) => {
+      this.borrowedBooks = books;
+      console.log(books)
+    }, (error) => {
+      console.error('Error fetching borrowed books:', error)
+    })
+  }
 
 }
