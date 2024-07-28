8684496a12cd7238803b54ced79fcdd1bc296585
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-28 16:48:07 +0200

    added functions related to borrowing books and added two functions which are shared

--- src/app/core/services/books.service.ts
+++ src/app/core/services/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { BehaviorSubject, Observable, Subject } from 'rxjs'
 import { Book } from '../models/book'
 import { HttpClient } from '@angular/common/http'
+import { BorrowedBook } from '../models/borrowed-book'
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,9 @@ import { HttpClient } from '@angular/common/http'
 
 export class BooksService{
 
-    selectedBook = new BehaviorSubject<Book | undefined>(undefined)
+    selectedBook = new BehaviorSubject<Book | null>(null)
+    bookDetailsComponentOpenedFrom = new BehaviorSubject<string | null>(null)
+    isActiveModalBehaviorSubject = new BehaviorSubject<boolean>(false)
 
     URL = "http://localhost:3000/books"
 
@@ -23,6 +26,29 @@ export class BooksService{
 
     addNewBook(newBook: Book): Observable<Book> {
         return this.http.post<Book>(this.URL, newBook)
+    }
+
+    getBookById(bookId: number): Observable<Book> {
+        return this.http.get<Book>(`${this.URL}/${bookId}`)
+    }
+
+    getBorrowedBooks(userId: number): Observable<Book[]> {
+        return this.http.get<Book[]>(`${this.URL}/borrowed-books/${userId}`)
+    }
+
+    returnBorrowedBook(userId: number, bookId: number): Observable<void> {
+        return this.http.delete<void>(`${this.URL}/borrowed-books/${userId}/${bookId}`)
+    }
+
+    openBookDetails(book: Book, component: string): void {
+        this.selectedBook.next(book)
+        this.bookDetailsComponentOpenedFrom.next(component)
+    }
+
+    detectSelectedBook(): void {
+        this.selectedBook.subscribe((data: Book | null) => {
+          data ? this.isActiveModalBehaviorSubject.next(true) : this.isActiveModalBehaviorSubject.next(false)
+        })
     }
 
 }
