66355607af3b44fd41adba66a511a24c9deeb7be
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-28 16:57:50 +0200

    added component control to let know the modal know which button display (return book or borrow). used functions from the service

--- src/app/book_library_app/books/book-details/book-details.component.ts
+++ src/app/book_library_app/books/book-details/book-details.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../../../core/services/books.service';
 import { Book } from '../../../core/models/book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent implements OnInit {
 
-  selectedBook?: Book
+  selectedBook?: Book | null
+  fromWhichComponentTheModalIsOpened: string | null = ''
 
   constructor(
     private bookService: BooksService
   ) {}
 
   ngOnInit(): void {
-    this.bookService.selectedBook.subscribe((book: Book | undefined) => {
+    this.detectSelectedBook()
+    this.detectFromWhichComponentModalIsOpened()
+  }
+
+  detectSelectedBook(): void {
+    this.bookService.selectedBook.subscribe((book: Book | null) => {
       this.selectedBook = book
     })
+  }
+
+  detectFromWhichComponentModalIsOpened(): void {
+    this.bookService.bookDetailsComponentOpenedFrom.subscribe((data: string | null) => {
+      this.fromWhichComponentTheModalIsOpened = data
+    })
+  }
+
+  closeModal(): void {
+    this.bookService.selectedBook.next(null)
+    this.bookService.bookDetailsComponentOpenedFrom.next(null)
   }
 
 }
