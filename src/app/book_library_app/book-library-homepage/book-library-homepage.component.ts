da6c18fa6fd35753ee5ef1cb49498c5bccc52206
kamilaltemimi <kamilaltemimi@gmail.com>
2024-07-29 13:00:57 +0200

    setted up a basic homepage

--- src/app/book_library_app/book-library-homepage/book-library-homepage.component.ts
+++ src/app/book_library_app/book-library-homepage/book-library-homepage.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../core/services/users.service';
+import { User } from '../../core/models/user';
 
 @Component({
   selector: 'app-book-library-homepage',
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './book-library-homepage.component.html',
   styleUrl: './book-library-homepage.component.scss'
 })
-export class BookLibraryHomepageComponent {
+export class BookLibraryHomepageComponent implements OnInit {
 
+  activeUser: User | null = null
+
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.usersService.activeUser.subscribe((data: User | null) => this.activeUser = data)
+  }
 }
